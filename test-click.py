89392861d6137707d112e4100fa2970c01f287b1
Martzyx <114746510+Martzyx@users.noreply.github.com>
2023-12-19 18:32:42 +0100

    first commit

---
+++ test-click.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time
+
+# Give you a few seconds to switch to the screen where you want the click to occur
+time.sleep(5)
+
+# Move the mouse to the specified coordinates and click
+pyautogui.click(1357.50390625, 434.81640625)
+time.sleep(1)
+pyautogui.click(678.98046875, 551.78125)
+time.sleep(1)
+pyautogui.click(426.55859375, 673.28515625)
+time.sleep(1)
