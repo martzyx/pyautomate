bb5103d7fd41ebe2987b689892f9b6cea9ec234f
Martzyx <114746510+Martzyx@users.noreply.github.com>
2023-12-19 18:39:53 +0100

    x-click coordinate finding

--- first-pyautogui.py
+++ first-pyautogui.py
@@ -1,9 +1,33 @@
-from pynput import mouse
+from pynput import mouse, keyboard
+
+x_pressed = False
 
 def on_click(x, y, button, pressed):
-    if pressed and button == mouse.Button.left:
+    global x_pressed
+    if pressed and button == mouse.Button.left and x_pressed:
         print(f"({x}, {y})")
 
-# Collect events until released
-with mouse.Listener(on_click=on_click) as listener:
-    listener.join()
+def on_press(key):
+    global x_pressed
+    try:
+        if key.char == 'x':  # Check if the key is 'x'
+            x_pressed = True
+    except AttributeError:
+        pass
+
+def on_release(key):
+    global x_pressed
+    try:
+        if key.char == 'x':
+            x_pressed = False
+    except AttributeError:
+        pass
+
+mouse_listener = mouse.Listener(on_click=on_click)
+mouse_listener.start()
+
+keyboard_listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+keyboard_listener.start()
+x
+mouse_listener.join()
+keyboard_listener.join()
